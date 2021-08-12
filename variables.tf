08b436fbfdfd2731265a1378200e5b4043732078
Nhat Bui <minhnhat992@gmail.com>
2021-08-12 11:31:37 -0400

    add default values

--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
-variable "project" {}
+variable "project" {
+  default = "nyt-mbcompdev-dev"
+}
 
-variable "credentials_file" {}
+variable "credentials_file" {
+  default = "creds/nyt-mbcompdev-dev-9728dd97d09d.json"
+}
 
 variable "region" {
   default = "us-east1"
